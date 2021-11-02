ec4ffb9df3adde31d31ff91d9da8992247a671ee
marcelo.biker.dev@gmail.com <marcelo.biker.dev@gmail.com>
2021-11-02 13:55:09 +0000

    initiated project structure, frontend and backend folders, React and Spring Boot

---
+++ backend/src/main/java/com/mysalescompany/cssapp/CssappApplication.java
@@ -0,0 +1,13 @@
+package com.mysalescompany.cssapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mysalescompany/cssapp/CssappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysalescompany.cssapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
